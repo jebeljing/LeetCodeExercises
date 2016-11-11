d24928a34db3482acc58cf4afeef01c671d38988
jebeljing <jebeljing@126.com>
2016-11-11 00:05:59 -0500

    160. Intersection of Two Linked Lists

---
+++ src/package100/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,57 @@
+package package100;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by jingshanyin on 11/11/16.
+ * 160. Intersection of Two Linked Lists
+ * Write a program to find the node at which the intersection of two singly linked lists begins.
+
+
+ For example, the following two linked lists:
+
+ A:          a1 → a2
+ ↘
+ c1 → c2 → c3
+ ↗
+ B:     b1 → b2 → b3
+ begin to intersect at node c1.
+
+
+ Notes:
+
+ If the two linked lists have no intersection at all, return null.
+ The linked lists must retain their original structure after the function returns.
+ You may assume there are no cycles anywhere in the entire linked structure.
+ Your code should preferably run in O(n) time and use only O(1) memory.
+ */
+public class IntersectionOfTwoLinkedLists {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        Set<ListNode> set = new HashSet<>();
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        while (!set.contains(headB) && headB != null) {
+            headB = headB.next;
+        }
+        if (headB != null) return headB;
+        return null;
+
+    }
+
+    //Definition for singly-linked list.
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
