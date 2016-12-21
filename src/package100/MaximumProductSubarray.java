5c7cbeb60f520175bf54ef7d90ac31eb6a9af076
jebeljing <jebeljing@126.com>
2016-12-20 22:43:49 -0500

    152. Maximum Product Subarray

---
+++ src/package100/MaximumProductSubarray.java
@@ -0,0 +1,29 @@
+package package100;
+
+/**
+ * Created by jingshanyin on 12/20/16.
+ * 152. Maximum Product Subarray
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+ For example, given the array [2,3,-2,4],
+ the contiguous subarray [2,3] has the largest product = 6.
+ */
+public class MaximumProductSubarray {
+
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        int max = nums[0];
+        int min = nums[0];
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int temp = max;
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+            min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+            if (max > result){
+                result = max;
+            }
+        }
+        return result;
+    }
+}
