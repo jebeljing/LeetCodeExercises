4c8b89d751708cba01e56ba7bda6b2b4c02ed8d4
jebeljing <jebeljing@126.com>
2017-01-15 20:57:20 -0500

    341. Flatten Nested List Iterator

---
+++ src/base/NestedInteger.java
@@ -0,0 +1,38 @@
+package base;
+
+import java.util.List;
+
+/**
+ * Created by jingshanyin on 1/15/17.
+ * 341. Flatten Nested List Iterator
+ *
+ * /**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+
+public interface NestedInteger {
+        // @return true if this NestedInteger holds a single integer, rather than a nested list.
+      public boolean isInteger();
+
+      // @return the single integer that this NestedInteger holds, if it holds a single integer
+      // Return null if this NestedInteger holds a nested list
+      public Integer getInteger();
+
+      // @return the nested list that this NestedInteger holds, if it holds a nested list
+      // Return null if this NestedInteger holds a single integer
+      public List<NestedInteger> getList();
+}
