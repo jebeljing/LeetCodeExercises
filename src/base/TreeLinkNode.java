d15d8bcab4c9eea114588717ee3ec101e4883afe
jebeljing <jebeljing@126.com>
2017-01-19 00:36:00 -0500

    116. Populating Next Right Pointers in Each Node

---
+++ src/base/TreeLinkNode.java
@@ -0,0 +1,13 @@
+package base;
+
+/**
+ * Created by jingshanyin on 1/19/17.
+ * 116. Populating Next Right Pointers in Each Node
+ *
+ */
+public class TreeLinkNode {
+
+    public TreeLinkNode left;
+    public TreeLinkNode right;
+    public TreeLinkNode next;
+}
